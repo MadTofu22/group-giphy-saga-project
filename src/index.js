80f00e6de45308acc41f77ceeceace55e373d15b
MadTofu22 <stutler.tom@gmail.com>
2020-11-05 14:02:20 -0600

    added saga and redux imports then connected the store and root saga

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+// Imports for Redux
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+// Imports for Sagas
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+
+
+
+// Setup the Redux store and link Sagas to it
+const sagaMiddleware = createSagaMiddleware();
+sagaMiddleware.run(giphySaga);
+const storeInstance = createStore(
+    combineReducers({
+        giphyReducer
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+);
+
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('react-root'));
